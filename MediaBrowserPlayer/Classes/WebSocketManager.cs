0dae4a4fef0fe138938563659f57ba66fdd7872c
faush01 <shaun@bluebit.com.au>
2014-08-14 14:48:34 +1000

    first cut

---
+++ MediaBrowserPlayer/Classes/WebSocketManager.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web;
+
+namespace MediaBrowserPlayer.Classes
+{
+
+    class WebSocketManager
+    {
+
+        private MessageWebSocket webSocket = null;
+
+        public WebSocketManager()
+        {
+
+        }
+
+        public void CloseWebSocket()
+        {
+            if(webSocket != null)
+            {
+                try
+                {
+                    webSocket.Close(0, "App Closing");
+                }
+                catch (Exception)
+                { }
+                webSocket = null;
+            }
+        }
+
+        public async void SetupWebSocket()
+        {
+                     
+            Uri server = new Uri("ws://localhost:8096/mediabrowser");
+            webSocket = new MessageWebSocket();
+            webSocket.Control.MessageType = SocketMessageType.Utf8;
+
+            webSocket.MessageReceived += MessageReceived;
+
+            webSocket.Closed += Closed;
+
+            await webSocket.ConnectAsync(server);
+
+            DataWriter messageWriter = new DataWriter(webSocket.OutputStream);
+
+            string identityMessage = "{\"MessageType\":\"Identity\", \"Data\":\"Windows RT|12345|0.0.1|MBP\"}";
+            messageWriter.WriteString(identityMessage);
+            await messageWriter.StoreAsync();
+
+        }
+
+        private void MessageReceived(MessageWebSocket sender, MessageWebSocketMessageReceivedEventArgs args)
+        {
+            try
+            {
+                using (DataReader reader = args.GetDataReader())
+                {
+                    reader.UnicodeEncoding = Windows.Storage.Streams.UnicodeEncoding.Utf8;
+                    string read = reader.ReadString(reader.UnconsumedBufferLength);
+
+                    Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        Frame rootFrame = Window.Current.Content as Frame;
+                        var p = rootFrame.Content as MainPage;
+                        p.LogMessage(read);
+
+                        bool isvisible = Window.Current.Visible;
+                        
+                    });
+
+
+                }
+            }
+            catch (Exception ex) // For debugging
+            {
+                WebErrorStatus status = WebSocketError.GetStatus(ex.GetBaseException().HResult);
+                // Add your specific error-handling code here.
+            }
+        }
+
+        private void Closed(IWebSocket sender, WebSocketClosedEventArgs args)
+        {
+            // You can add code to log or display the code and reason
+            // for the closure (stored in args.Code and args.Reason)
+
+            // This is invoked on another thread so use Interlocked 
+            // to avoid races with the Start/Close/Reset methods.
+            if (webSocket != null)
+            {
+                MessageWebSocket ws = Interlocked.Exchange(ref webSocket, null);
+                if (ws != null)
+                {
+                    ws.Dispose();
+                }
+                Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    Frame rootFrame = Window.Current.Content as Frame;
+                    var p = rootFrame.Content as MainPage;
+                    p.LogMessage("SocketClosed");
+                });
+            }
+        }
+
+        private byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+
+    }
+}
---
+++ MediaBrowserPlayer/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MediaBrowserPlayer
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            mainWebPage.NavigationStarting += OnNavigate;
+
+            Uri mb = new Uri("http://localhost:8096/mediabrowser");
+            mainWebPage.Navigate(mb);
+        }
+
+        private void OnNavigate(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            Uri destination = args.Uri;
+
+            // block if not to media portal
+            if(destination.Host != "localhost")
+            {
+                args.Cancel = true;
+            }
+
+        }
+
+        public void LogMessage(string data)
+        {
+            notificationBox.Text = notificationBox.Text + data + "\n";
+        }
+    }
+}
